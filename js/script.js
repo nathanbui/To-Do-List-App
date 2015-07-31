7d11af42ebc1461a982df6ce77348bbe7bb02a8d
Nathan Bui <nathan.bui@uqconnect.edu.au>
2015-07-31 16:29:43 +1000

    Sidebar MyList

--- js/script.js
+++ js/script.js
@@ -54,6 +54,9 @@ $(document).ready(function() {
 		updateMyList();
 		updateListItemsLength();
 		updateListItems();
+		clearMyListInput();
+		debug();
+		// TODO: toggle back to new button WHEN 'enter' or clicked
 	}
 	
 	function showCurrentListItems () {
@@ -85,28 +88,32 @@ $(document).ready(function() {
 		}
 		updateListItems();
 	}
+
+	function clearMyListInput() {
+		$(".myList-newInput").val("");
+	}
+
+	function toggleToMyListButton() {
+		$(".myList-newButton").show();
+		$(".myList-newForm").hide();
+	}
+
+	function toggleToMyListForm() {
+		$(".myList-newButton").hide();
+		$(".myList-newForm").show();
+	}
 	
 	// Toggle lists and show their list items
 	$("#myLists").on('click', '.myLists-item', function() {
 		currentMyList = $(this).data("list");
 		showCurrentListItems();
+		$(".myLists-item").removeClass("highlight");
+		$(this).addClass("highlight");
 	});
 
 	// Add new list
 	$(".sidebar").on('click', '.myList-newButton', function() {
-		$(this).hide();
-		$(".myList-newForm").show();
-	});
-
-	// When 'enter' key is pressed
-	$(".sidebar").find(".myList-newInput").keydown(function(e) {
-		if (e.which == "13") {
-			e.preventDefault();
-
-			if (checkMyListInput() == true) {
-				addMyListItem($(".myList-newInput").val());
-			}
-		}
+		toggleToMyListForm();
 	});
 
 	$("#listItem-input").keydown(function(e) {
@@ -114,6 +121,34 @@ $(document).ready(function() {
 			e.preventDefault();
 			addNewListItem();
 		}
+	});
+
+	// When 'enter' key is pressed on My List input
+	$(".sidebar").find(".myList-newInput").keydown(function(e) {
+		if (e.which == "13") {
+			e.preventDefault();
+
+			if (checkMyListInput() == true) {
+				addMyListItem( $(this).val() );
+			}
+		}
+	});
+
+	// When MyList Confirm Button is Clicked
+	$("#myList-confirmButton").on("click", function() {
+		// hide form and show new list button
+		if (checkMyListInput() == true) {
+			addMyListItem($(".myList-newInput").val());
+			toggleToMyListButton();
+		}
+	});
+
+	// When MyList Cancel Button is Clicked
+	$("#myList-cancelButton").on("click", function() {
+		// hide form and show new list button
+		clearMyListInput(); // Clear input field
+		$(".myList-newForm").hide();
+		$(".myList-newButton").show();
 	});
 
 
@@ -127,23 +162,18 @@ $(document).ready(function() {
 		console.log("start INIT");
 		console.log(myListArray);
 		console.log("end INIT");
-
 	}
 
 	init();
 
 	function debug(){
+		console.log("START DEBUG");
 		console.log("listItemArray: ");
 		console.log(listItemArray);
 		console.log("listItemLength: ");
 		console.log(listItemLength);
+		console.log("END DEBUG");
 	}
-	// DEBUG
-	/*console.log("myList");
-	console.log(myListArray);
-	console.log("currentListItemArray");
-	console.log(currentListItemArray);*/
-	
 });
 /* ADDING VALUE FROM FORM TO ITEM
 $(document).ready(function() {
