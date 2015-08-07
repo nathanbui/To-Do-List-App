cf302b5d6a64a428619a5d2e7726cd9e1180a386
Nathan Bui <nathan.bui@uqconnect.edu.au>
2015-08-07 16:38:54 +1000

    To Do App Complete
    Completed Project

--- js/script.js
+++ js/script.js
@@ -7,6 +7,10 @@ $(document).ready(function() {
 	var listItemLength = 0;
 	var currentListItem = 0;
 	
+	/*
+	 * UPDATE FUNCTIONS
+	 */
+
 	function updateMyListLength() {
 		myListLength = $("#myLists").children(".myLists-item").length;
 	}
@@ -29,6 +33,21 @@ $(document).ready(function() {
 		}
 	}
 
+	function updateAllLists() {
+		updateMyListLength();
+		updateMyList();
+		updateListItemsLength();
+		updateListItems();
+	}
+
+	function updateListItemTitle() {
+		$(".listItem-title").html( findCurrentMyList().find(".myList-title").html() );
+	}
+
+	/*
+	 * MY LIST FUNCTIONS
+	 */
+
 	function generateMyListId() {
 		updateMyList();
 		return myListArray.length;
@@ -36,7 +55,7 @@ $(document).ready(function() {
 
 	function addMyListItem(title) {
 		// myList template
-		var myListTemp = $(".myLists-itemTemplate").clone();
+		var myListTemp = $(".myLists-itemTemplate").clone(true);
 		var myListId = generateMyListId();
 
 		myListTemp.data("list",myListId);
@@ -47,22 +66,67 @@ $(document).ready(function() {
 		myListTemp.removeClass("hide myLists-itemTemplate").addClass("myLists-item").appendTo( $("#myLists") ) ;
 		// Add empty list to the sub list items on the right side
 		$("#listItems-big").append( $("<ul data-list-item=\"\"></ul>").data("list-item",myListId) );
-		addListenerNewMyList();
+		updateAllLists();
+		hideMyListEmpty();
 	}
 
-	function addListenerNewMyList() {
-		updateMyListLength();
-		updateMyList();
-		updateListItemsLength();
-		updateListItems();
-		clearMyListInput();
-		addListenerMyListDelete();
-		addListenerMyListEdit();
-		addListenerMyEditCancel();
-		addListenerMyListEditConfirm();
-		addListenerMyListEditEnter();
+	function toggleToMyListButton() {
+		$(".myList-newButton").show();
+		$(".myList-newForm").hide();
 	}
-	
+
+	function toggleToMyListForm() {
+		$(".myList-newButton").hide();
+		$(".myList-newForm").show();
+	}
+
+	function checkMyListEmpty() {
+		var message = $(".myList-empty");
+		var myLists = $(".myLists-item");
+		
+		if (myLists.length === 0) {
+			message.show();
+		}
+	}
+
+	function hideMyListEmpty() {
+		$(".myList-empty").hide();
+	}
+
+	function hideListItemTitleButton() {
+		$(".listItem-title").hide();
+		$(".listItem-newButton").hide();
+		$(".listItem-form").hide();
+	}
+
+	function toggleListItemTitleButton() {
+		$(".listItem-title").show();
+		$(".listItem-newButton").show();
+	}
+
+	function checkHideListItemTitleButton(currentElement) {
+		if ( $(currentElement).closest(".myList-main").hasClass("highlight") ) {
+			hideListItemTitleButton();
+		}
+	}
+
+	/*
+	 * LIST ITEM FUNCTIONS
+	 */
+	function addNewListItem(userInput) {
+		var listItemGroup = $("#listItems-big").children("ul");
+		var listItemTemp = $(".listItem-singleTemplate").clone(true).removeClass("listItem-singleTemplate");
+		var listItemProcessed = listItemTemp.find(".listItem-text").html(userInput).closest(".listItem-single");
+
+		for (var i = 0; i < listItemGroup.length; i++) {
+			if ( $(listItemGroup[i]).data("list-item") == currentMyList ) {
+				$(listItemGroup[i]).append(listItemProcessed);
+				break;
+			}
+		}
+		updateListItems();
+	}
+
 	function showCurrentListItems () {
 		$("#listItems-big").children("ul").hide();
 		var target = $("#listItems-big").children("ul").data("list-item");
@@ -76,35 +140,115 @@ $(document).ready(function() {
 		}
 	}
 
+	function toggleNewItemForm() {
+		$(".listItem-newButton").hide();
+		$(".listItem-form").show();
+	}
+
+	function resetNewItemForm() {
+		$(".listItem-newButton").show();
+		$(".listItem-form").hide();
+		$("#listItem-input").val("");
+	}
+
 	function checkMyListInput () {
 		return ( $(".myList-newInput").val().trim().length > 0 );
 	}
 
-	function addNewListItem() {
-		var userInput = $("#listItem-input").val();
-		var listItemGroup = $("#listItems-big").children("ul");
+	function findListItemNumber(number) {
+		var listItem = $("#listItems-big").children("ul");
 
-		for (var i = 0; i < listItemGroup.length; i++) {
-			if ( $(listItemGroup[i]).data("list-item") == currentMyList ) {
-				$(listItemGroup[i]).append("<li>"+userInput+"</li>");
+		for (var i = 0; i < listItem.length; i++) {
+			if ( $(listItem[i]).data().listItem == number ) {
+				return listItem[i];
+			}
+		}
+	}
+
+	function findCurrentMyList() {
+		var myList = $(".myLists-item");
+		
+		for (var i = 0; i < myList.length; i++) {
+			if ( $(myList[i]).data().list == currentMyList ) {
+				return $(myList[i]);
 				break;
 			}
 		}
-		updateListItems();
 	}
 
-	function clearMyListInput() {
-		$(".myList-newInput").val("");
+	function checkMyListTitle(currentElement) {
+		var titleInput = $(currentElement).closest(".myList-editTemplate").find(".myList-editTitle");
+		var trimmedTitle = titleInput.val().trim();
+		var listTitle = $(currentElement).closest(".myLists-item").find(".myList-title");
+
+		if (trimmedTitle.length > 0) {
+			listTitle.html(trimmedTitle);
+			resetCurrentMyListEditMode( $(currentElement) );
+		}
 	}
 
-	function toggleToMyListButton() {
-		$(".myList-newButton").show();
-		$(".myList-newForm").hide();
+	/*
+	 * CLICK FUNCTIONS
+	 */
+	
+	// Toggle lists and show their list items
+	function addListenerShowMyLists() {
+		$("#myLists").on('click', '.myList-main', function() {
+			currentMyList = $(this).closest(".myLists-item").data("list");
+			showCurrentListItems();
+			$(".myList-main").removeClass("highlight");
+			$(this).addClass("highlight");
+			updateListItemTitle();
+			resetNewItemForm();
+			toggleListItemTitleButton();
+		});
 	}
 
-	function toggleToMyListForm() {
-		$(".myList-newButton").hide();
-		$(".myList-newForm").show();
+	function addListenerListItemInput() {
+		$("#listItem-input").keydown(function(e) {
+			if (e.which == "13") {
+				e.preventDefault();
+				var userInput = $(this).val().trim();
+				
+				if (userInput.length > 0) {
+					addNewListItem(userInput);
+					$(this).val("");
+				}
+			}
+		});
+	}
+
+	function addListenerNewItemButton() {
+		$(".listItem-newButton").on("click", function() {
+			toggleNewItemForm();
+			$("#listItem-input").focus();
+		});
+	}
+
+	function addListenerMyListEditCancel() {
+		// Cancel for My List edit input
+		$(".myList-editCancel").on("click", function() {
+			resetCurrentMyListEditMode($(this));
+		});
+	}
+
+	function addListenerMyListEditEnter() {
+		// When 'enter' key is pressed when editting title
+		$(".myList-editTitle").keydown(function(e) {
+			if (e.which == "13") {
+				console.log("enter is press");
+				e.preventDefault();
+				checkMyListTitle( $(this) );
+				updateListItemTitle();
+			}
+		});
+	}
+
+	function addListenerMyListEditConfirm() {
+		$(".myList-editConfirm").on("click", function() {
+			checkMyListTitle( $(this) );
+			updateListItemTitle();
+		});
 	}
 
 	function addListenerMyListDelete() {
@@ -119,53 +263,10 @@ $(document).ready(function() {
 			$(this).closest(".myLists-item").remove();
 			updateMyList();
 			updateMyListLength();
+			checkMyListEmpty();
+			checkHideListItemTitleButton( $(this) );
 		});
 	}
-
-	function findListItemNumber(number) {
-		var listItem = $("#listItems-big").children("ul");
-
-		for (var i = 0; i < listItem.length; i++) {
-			if ( $(listItem[i]).data().listItem == number ) {
-				return listItem[i];
-			}
-		}
-	}
-
-	// Reset all other My List edit modes
-	function resetMyListEditMode() {
-		$(".myList-editTemplate").hide();
-		$(".myList-main").show();
-	}
-	
-	// Toggle lists and show their list items
-	$("#myLists").on('click', '.myList-main', function() {
-		currentMyList = $(this).closest(".myLists-item").data("list");
-		showCurrentListItems();
-		$(".myList-main").removeClass("highlight");
-		$(this).addClass("highlight");
-	});
-
-	$("#listItem-input").keydown(function(e) {
-		if (e.which == "13") {
-			e.preventDefault();
-			addNewListItem();
-		}
-	});
-
-	// When MyList Confirm Button is Clicked
-	$("#myList-confirmButton").on("click", function() {
-		// hide form and show new list button
-		if (checkMyListInput() == true) {
-			addMyListItem($(".myList-newInput").val());
-			toggleToMyListButton();
-		}
-	});
-
-	// Removing list item
-	$(".listItem-remove").on("click", function() {
-		$(this).closest(".listItem-single").remove();
-	});
 
 	// Edit My List Title
 	function addListenerMyListEdit() {
@@ -184,47 +285,68 @@ $(document).ready(function() {
 		});
 	}
 
-	function addListenerMyListEditConfirm() {
-		$(".myList-editConfirm").on("click", function() {
-			checkMyListTitle( $(this) );
-			console.log("save  is press");
+	// Removing list item
+	function addListenerListItemRemove() {
+		$(".listItem-remove").on("click", function() {
+			$(this).closest(".listItem-single").remove();
+			resetNewItemForm();
 		});
 	}
 
-	function addListenerMyListEditEnter() {
-		// When 'enter' key is pressed when editting title
-		$(".myList-editTitle").keydown(function(e) {
-			if (e.which == "13") {
-				console.log("enter is press");
-				e.preventDefault();
-				checkMyListTitle( $(this) );
+	function addListenerAddListButton() {
+		// When MyList Cancel Button is Clicked
+		$("#myList-cancelButton").on("click", function() {
+			// hide form and show new list button
+			clearMyListInput(); // Clear input field
+			$(".myList-newForm").hide();
+			$(".myList-newButton").show();
+		});
+	}
+
+	// When MyList Confirm Button is Clicked
+	function addListenerConfirmNewMyList() {
+		$("#myList-confirmButton").on("click", function() {
+			// hide form and show new list button
+			if (checkMyListInput() == true) {
+				addMyListItem($(".myList-newInput").val());
+				toggleToMyListButton();
 			}
 		});
 	}
 
-	function checkMyListTitle(currentElement) {
-		var titleInput = $(currentElement).closest(".myList-editTemplate").find(".myList-editTitle");
-		var trimmedTitle = titleInput.val().trim();
-		var listTitle = $(currentElement).closest(".myLists-item").find(".myList-title");
+	// When 'enter' key is pressed when adding a new My List
+	function addListenerMyListEnterNewList() {
+		$(".sidebar").find(".myList-newInput").keydown(function(e) {
+			if (e.which == "13") {
+				e.preventDefault();
 
-		if (trimmedTitle.length > 0) {
-			listTitle.html(trimmedTitle);
-			resetCurrentMyListEditMode( $(currentElement) );
-		}
-	}
-
-	function addListenerMyEditCancel() {
-		// Cancel for My List edit input
-		$(".myList-editCancel").on("click", function() {
-			resetCurrentMyListEditMode($(this));
+				if (checkMyListInput() == true) {
+					addMyListItem( $(this).val() );
+					$(".myList-newForm").hide();
+					$(".myList-newButton").show();
+				}
+			}
 		});
 	}
 
+	function addListenerListItemCancel() {
+		$("#listItem-cancel").on("click", function() {
+			resetNewItemForm();
+		});
+	}
 
-	//TODO: when list item checkbox click, toggle up moves up
-	$(".listItem-checkbox").on("click", function() {
-		$(this).closest(".listItem-single").slideUp();
-	});
+	function addListenerListItemButton() {
+		$("#listItem-confirm").on("click", function() {
+			var inputField = $("#listItem-input");
+			var userInput = $("#listItem-input").val().trim();
+
+			if (userInput.length > 0) {
+				addNewListItem(userInput);
+				inputField.val("");
+				inputField.focus();
+			}
+		});
+	}
 
 	/*
 	 * RESET FUNCTIONS
@@ -235,37 +357,36 @@ $(document).ready(function() {
 		$(currentElement).closest(".myLists-item").find(".myList-editTemplate").hide();
 	}
 
+	function clearMyListInput() {
+		$(".myList-newInput").val("");
+	}
+
+	// Reset all other My List edit modes
+	function resetMyListEditMode() {
+		$(".myList-editTemplate").hide();
+		$(".myList-main").show();
+	}
+
 	/*
 	 * RUN ONCE 
 	 */
-	 
-	// When MyList Cancel Button is Clicked
-	$("#myList-cancelButton").on("click", function() {
-		// hide form and show new list button
-		clearMyListInput(); // Clear input field
-		$(".myList-newForm").hide();
-		$(".myList-newButton").show();
-	});
 
-	// When 'enter' key is pressed when adding a new My List
-	$(".sidebar").find(".myList-newInput").keydown(function(e) {
-		if (e.which == "13") {
-			e.preventDefault();
-
-			if (checkMyListInput() == true) {
-				addMyListItem( $(this).val() );
-				$(".myList-newForm").hide();
-				$(".myList-newButton").show();
-			}
-		}
-	});
+	// List item completed toggle delete
+	function addListenerListItemComplete() {
+		$(".listItem-checkbox").on("click", function() {
+			$(this).closest(".listItem-single").slideUp();
+		});
+	}
 
 	// Add new list
-	$(".sidebar").on('click', '.myList-newButton', function() {
-		toggleToMyListForm();
-		resetMyListEditMode();
-		$(".myList-newInput").focus();
-	});
+	function addListenerNewMyList() {
+		$(".sidebar").on('click', '.myList-newButton', function() {
+			toggleToMyListForm();
+			resetMyListEditMode();
+			clearMyListInput();
+			$(".myList-newInput").focus();
+		});
+	}
 
 	function init() {
 		updateMyListLength();
@@ -275,23 +396,22 @@ $(document).ready(function() {
 		showCurrentListItems();
 		addListenerMyListDelete();
 		addListenerMyListEdit();
-		addListenerMyEditCancel();
+		addListenerMyListEditCancel();
 		addListenerMyListEditConfirm();
 		addListenerMyListEditEnter();
-
-		console.log("start INIT");
-		console.log(myListArray);
-		console.log("end INIT");
+		addListenerAddListButton();
+		addListenerListItemRemove();
+		addListenerShowMyLists();
+		addListenerMyListEnterNewList();
+		addListenerConfirmNewMyList();
+		updateListItemTitle();
+		addListenerNewItemButton();
+		addListenerNewMyList();
+		addListenerListItemCancel();
+		addListenerListItemInput();
+		addListenerListItemButton();
+		addListenerListItemComplete();
 	}
 
 	init();
-
-	function debug(){
-		console.log("START DEBUG");
-		console.log("listItemArray: ");
-		console.log(listItemArray);
-		console.log("listItemLength: ");
-		console.log(listItemLength);
-		console.log("END DEBUG");
-	}
 });
